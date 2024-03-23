53eaa97f62c2f826d0f9f4d1451b25bdaeda2b5d
1ambuj <110759439+1ambuj@users.noreply.github.com>
2024-03-23 17:07:20 +0530

    Add Functionality of like and dislike and save post

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,40 +1,51 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { useNavigate } from "react-router-dom"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
+import { useUserContext } from "@/context/AuthContext"
+import { useToast } from "../ui/use-toast"
+import { useCreatePost } from "@/lib/react-query/queryAndMutations"
 
-const formSchema = z.object({
-    username: z.string().min(2, {
-      message: "Username must be at least 2 characters.",
-    }),
-  })
+type PostFormProps={
+  post?: Models.Document;
+}
 
-const PostForm = () => {
+const PostForm = ({post}: PostFormProps) => {
+  const { mutateAsync: createPost, isPending: isLoadingCreate} =  useCreatePost();
+  const {user} = useUserContext();
+  const { toast } = useToast()
+  const navigate = useNavigate();
     // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      username: "",
+      caption : post? post?.caption : "",
+      file: [],
+      location: post ? post?.location : "",
+      tags: post ? post.tags.join(',') : ''
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+   const newPost = await createPost({
+    ...values,
+    userId : user.id
+   })
+   if(!newPost){
+      toast({
+        title: 'please try again'
+      })
+   }
+   navigate("/");
   }
   return (
     <Form {...form}>
@@ -62,7 +73,10 @@ const PostForm = () => {
             <FormControl>
                <Input type="text" className="shad-input"/>
             </FormControl>
-            <FileUploader />
+            <FileUploader 
+               fieldChange={field.onChange}
+               mediaUrl = {post?.imageUrl}
+            />
             <FormMessage  className="shad-form_message"/>
           </FormItem>
         )}
@@ -74,9 +88,9 @@ const PostForm = () => {
           <FormItem>
             <FormLabel className="shad-form_label">Add Location</FormLabel>
             <FormControl>
-               <Input type="text" className="shad-input"/>
+               <Input type="text" className="shad-input" {...field}/>
             </FormControl>
-            <FileUploader />
+           
             <FormMessage  className="shad-form_message"/>
           </FormItem>
         )}
@@ -92,9 +106,9 @@ const PostForm = () => {
                type="text" 
                className="shad-input"
                placeholder="Js, React, NextJs"
+               {...field}
                />
             </FormControl>
-            <FileUploader />
             <FormMessage  className="shad-form_message"/>
           </FormItem>
         )}
--- src/lib/react-query/queryAndMutations.ts
+++ src/lib/react-query/queryAndMutations.ts
@@ -4,8 +4,11 @@ import {
    useQueryClient,
    useInfiniteQuery,
 }from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
+
+
 
 export const useCreateUserAccount = () =>{
     return useMutation({
@@ -24,5 +27,98 @@ export const useSignInAccount = () =>{
 export const useSignOutAccount = () =>{
     return useMutation({
         mutationFn: signOutAccount
+    })
+}
+
+
+// ##################Post Queries############
+
+export const useCreatePost =() =>{
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess :()=>{
+          queryClient.invalidateQueries({
+            queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+          })
+        }
+  
+    })
+  }
+export const useGetRecentPosts = () =>{
+   return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn:getRecentPosts,
+   })
+  }
+
+
+export const useLikePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({
+        postId,
+        likesArray,
+      }: {
+        postId: string;
+        likesArray: string[];
+      }) => likePost(postId, likesArray),
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+
+export const useSavePost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: ({ postId , userId}: { userId: string; postId: string }) =>
+        savePost( postId, userId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+export const useDeleteSavedPost = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_POSTS],
+        });
+        queryClient.invalidateQueries({
+          queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        });
+      },
+    });
+  };
+
+export const  useGetCurrentUser = () =>{
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+        queryFn : getCurrentUser
     })
 }
